a3b50560263d2be9b83fe31182e4f633904327c6
Donald Chen <donaldchen@Donalds-MacBook-Pro.local>
2020-06-09 20:26:39 -0700

    set up test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDesAatuiqg8O6LobxBwufbwul9dB2pDsQ",
-    authDomain: "expensify-2c9f4.firebaseapp.com",
-    databaseURL: "https://expensify-2c9f4.firebaseio.com",
-    projectId: "expensify-2c9f4",
-    storageBucket: "expensify-2c9f4.appspot.com",
-    messagingSenderId: "1062655689803",
-    appId: "1:1062655689803:web:64ba516583a8357a31b9ce",
-    measurementId: "G-EBLDG5ZJ20"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
