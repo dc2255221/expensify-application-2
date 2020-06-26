8cca357141e20a8afe86ed4df99953d92aef7bf3
Donald Chen <donaldchen@Donalds-MacBook-Pro.local>
2020-06-26 01:01:08 -0700

    Add public route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,8 +6,8 @@ import LoginPage from '../components/LoginPage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
@@ -17,11 +17,10 @@ const AppRouter = () => (
         <div>
             {/* <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
             <Switch> 
-                <Route exact path="/" component={LoginPage}/>
+                <PublicRoute exact path="/" component={LoginPage}/>
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage}/>
                 <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
-                <Route path="/help" component={HelpPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
